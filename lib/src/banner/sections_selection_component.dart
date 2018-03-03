8ff5b6ab7e47b06bc4c3569f48292cedf7925ef0
jbaxe2 <jbaxe2@gmail.com>
2018-03-02 19:12:00 -0500

    2018-03-02, 19:12

---
+++ lib/src/banner/sections_selection_component.dart
@@ -0,0 +1,36 @@
+library plato.angular.components.banner.sections.selection;
+
+import 'dart:async' show Future;
+
+import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
+
+import 'section.dart';
+
+import 'sections_service.dart';
+
+/// The [SectionsSelectionComponent] class...
+@Component(
+  selector: 'sections-selection',
+  templateUrl: 'sections_selection_component.html',
+  directives: const [CORE_DIRECTIVES, materialDirectives],
+  providers: const [SectionsService]
+)
+class SectionsSelectionComponent {
+  List<Section> sections;
+
+  List<Section> selectedSections;
+
+  final SectionsService sectionsService;
+
+  /// The [SectionsSelectionComponent] constructor...
+  SectionsSelectionComponent (this.sectionsService) {
+    sections = new List<Section>();
+    selectedSections = new List<Section>();
+  }
+
+  /// The [retrieveSections] method...
+  Future retrieveSections() async {
+    ;
+  }
+}
